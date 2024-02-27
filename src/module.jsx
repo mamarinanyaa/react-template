dbb163e21ab5505bc5e13800d688e64e4db1b226
Ann <mamarina.ania@yandex.ru>
2024-02-27 13:44:50 +0300

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import daisy from './img/daisy.jpg'
+
+const Module = () => (
+    <>
+    <h1>Test Module</h1>
+    <img width = {500} src = {daisy}></img>
+    </>
+)
+
+export default Module;
